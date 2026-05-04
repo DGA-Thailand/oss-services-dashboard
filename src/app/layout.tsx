6802139cfad6afd443dcdb91abd04ea0720c83d1
Asis Unyapoth <asisunyapoth@192.168.1.15>
2026-05-04 11:17:35 +0700

    feat: add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Anuphan } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const anuphan = Anuphan({
@@ -23,6 +24,15 @@ export default function RootLayout({
     <html lang="th" className={anuphan.className}>
       <body className="antialiased">
         {children}
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-N0N990GGX7" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-N0N990GGX7');
+          `}
+        </Script>
       </body>
     </html>
   );
